c501c8b33a91f7d691d4b56e252e1dd4cd0415cd
Rick <unrickzyn@gmail.com>
2023-04-22 14:31:58 -0600

    Version no responsive del Encriptador

---
+++ script.js
@@ -0,0 +1,83 @@
+/*cancelar click derecho*/
+function disableIE() {
+  if (document.all) {
+      return false;
+  }
+}
+function disableNS(e) {
+  if (document.layers || (document.getElementById && !document.all)) {
+      if (e.which==2 || e.which==3) {
+          return false;
+      }
+  }
+}
+if (document.layers) {
+  document.captureEvents(Event.MOUSEDOWN);
+  document.onmousedown = disableNS;
+} 
+else {
+  document.onmouseup = disableNS;
+  document.oncontextmenu = disableIE;
+}
+document.oncontextmenu=new Function("return false");
+
+
+/*area de encriptado*/
+function encriptar () {
+  var frase = document.getElementById("textoPorEncriptar").value.toLowerCase();
+  
+ 
+  var textoPorEncriptar = frase.replace(/e/img, "enter");
+    var textoPorEncriptar = textoPorEncriptar.replace(/o/img, "ober");
+    var textoPorEncriptar = textoPorEncriptar.replace(/i/img, "imes");
+    var textoPorEncriptar = textoPorEncriptar.replace(/a/img, "ai");
+    var textoPorEncriptar = textoPorEncriptar.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoPorEncriptar;
+
+    
+         document.getElementById("esconder").style.display = "none"
+   
+          
+  
+}
+
+
+function desEncriptar () {
+  var frase = document.getElementById("textoPorEncriptar").value.toLowerCase();
+  
+  var textoDesencriptado = frase.replace(/ufat/img, "u");
+    textoDesencriptado = textoDesencriptado.replace(/ai/img, "a");
+    textoDesencriptado = textoDesencriptado.replace(/imes/img, "i");
+    textoDesencriptado = textoDesencriptado.replace(/ober/img, "o");
+    textoDesencriptado = textoDesencriptado.replace(/enter/img, "e");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoDesencriptado;
+
+    document.getElementById("esconder").style.display = "none"
+} 
+
+
+  
+  function copiar() {
+
+    var copyText = document.getElementById("textoDesencriptado");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+   
+    var divAlerta = document.getElementById('alerta-flotante');
+    divAlerta.style.display = 'block';
+
+    document.addEventListener('mousemove', function(e) {
+      divAlerta.style.top = e.clientY + 'px';
+      divAlerta.style.left = e.clientX + 'px';
+    });
+
+    setTimeout(function() {
+      divAlerta.style.display = 'none';
+    }, 2000);
+  }
+
+
+  
